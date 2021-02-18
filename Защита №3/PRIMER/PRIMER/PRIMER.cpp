3c972d89a9e8ac2f8de0bab91350fb8bfc82e7eb
Vadim3006 <79245655+Vadim3006@users.noreply.github.com>
2021-02-18 09:57:09 +0300

    Add files via upload

---
+++ Защита №3/PRIMER/PRIMER/PRIMER.cpp
@@ -0,0 +1,24 @@
+﻿#include "windows.h"
+#include <iostream>
+#include <tlhelp32.h>
+#include "string.h"
+
+
+int main()
+{
+
+	HANDLE consoleOutput;
+	COORD cursorPos;
+
+	// Получаем хэндл консоли 
+	consoleOutput = GetStdHandle(/*STD_OUTPUT_HANDLE*/2);
+
+	// Задаем координаты курсора и перемещаем курсор
+	cursorPos.X = 30;
+	cursorPos.Y = 3;
+	SetConsoleCursorPosition(consoleOutput, cursorPos);
+
+	// Выводим строку
+	printf("Test string at position (30, 3)");
+}
+
---
+++ Защита №3/Задание 2 (table)/Задание 2 (table)/Задание 2 (table).cpp
@@ -0,0 +1,56 @@
+﻿#include "windows.h"
+#include <iostream>
+#include <tlhelp32.h>
+#include "string.h"
+
+typedef BOOL(WINAPI* SETCONSOLEFONT)(HANDLE, DWORD);     
+SETCONSOLEFONT SetConsoleFont;
+using namespace std;
+const WORD colors[] =
+{
+0xF6
+};
+
+int main()
+{
+
+
+	HANDLE hstdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	WORD   index = 0;
+
+	
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(hstdout, &csbi);
+
+
+
+	while (index < 1 )
+	{
+		SetConsoleTextAttribute(hstdout, colors[index]);
+		cout << "     Surname      ";
+		cout << "     Group        " << endl;
+		cout << "     Petrov       ";
+		cout << "       1          " << endl;
+		cout << "     Solovev      ";
+		cout << "       2          " << endl;
+		cout << "     Radivill     ";
+		cout << "       3          " << endl;
+		cout << "     Vrublevskiy  ";
+		cout << "       4          " << endl;
+		cout << "     Shelashen    ";
+		cout << "       5          " << endl;
+		cout << "     Kalistratov  ";
+		cout << "       6          " << endl;
+
+
+		if (++index > sizeof(colors) / sizeof(colors[0]))
+			break;
+	}
+
+	
+
+
+	SetConsoleTextAttribute(hstdout, csbi.wAttributes);
+	getchar();
+	return 0;
+}
